cc9ecc41e3ae50c481de672707d128f574a6fc8b
Pavel Korshikov <korshikov@unicodemail.com>
2020-06-28 20:27:36 +0200

    Project Hello world!

---
+++ learning-jbehave/src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by My on 6/28/2020.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
+
+
---
+++ learning-jbehave/src/test/java/MainTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by My on 6/28/2020.
+ */
+class MainTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+
+    }
+
+}
